f9f98e0badaabfa4a64ee641dc236100e304b666
arskdj <68353557+arskdj@users.noreply.github.com>
2020-08-09 02:01:04 +0300

    fix spacing

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -2,70 +2,69 @@ import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
 const Anecdote = ({text, points}) => (
-	<>
-	<p> <em> {text} </em> </p>
-	<p> has <strong> {points} </strong> votes </p>
-	</>
+    <>
+        <p> <em> {text} </em> </p>
+        <p> has <strong> {points} </strong> votes </p>
+    </>
 )
 
 const App = ({anecdotes}) => {
-	const [selected, setSelected] = useState(0)
-	const [points, setPoints] = useState(Array(anecdotes.length).fill(0))
+    const [selected, setSelected] = useState(0)
+    const [points, setPoints] = useState(Array(anecdotes.length).fill(0))
 
 
-	const getRandomIndex = () => {
-		const n = Math.random() * 1000
-		const index =parseInt(n % anecdotes.length) 
-		setSelected(index)
-	}
+    const getRandomIndex = () => {
+        const n = Math.random() * 1000
+        const index =parseInt(n % anecdotes.length) 
+        setSelected(index)
+    }
 
-	const vote = () => {
-		let copy = [...points]
-		copy[selected] += 1
-		setPoints(copy)
-	}
+    const vote = () => {
+        let copy = [...points]
+        copy[selected] += 1
+        setPoints(copy)
+    }
 
-	const max_index = (() => {
-		let max = 0
-		let index = 0
+    const max_index = (() => {
+        let max = 0
+        let index = 0
 
-		for ( let i=0; i<points.length; i++){
-			if (max < points[i]){
-				max = points[i]
-				index = i
-			}
-		}
+        for (let i=0; i<points.length; i++){
+            if (max < points[i]){
+                max = points[i]
+                index = i
+            }
+        }
 
-		return index
-	})()
+        return index
+    })()
 
 
-	//debugger
+    //debugger
 
-	return (
-		<div>
-			<h1> Anecdote of the day </h1>
-			<button onClick={vote}> vote </button>
-			<button onClick={getRandomIndex}> next anecdote </button>
-			<Anecdote text={anecdotes[selected]} points={points[selected]} />
+    return (
+        <div>
+            <h1> Anecdote of the day </h1>
+            <button onClick={vote}> vote </button>
+            <button onClick={getRandomIndex}> next anecdote </button>
+            <Anecdote text={anecdotes[selected]} points={points[selected]} />
 
-			<h1> Anecdote with most votes </h1>
-			<Anecdote text={anecdotes[max_index]} points={points[max_index]} />
-
-		</div>
-	)
+            <h1> Anecdote with most votes </h1>
+            <Anecdote text={anecdotes[max_index]} points={points[max_index]} />
+        </div>
+    )
 }
 
 const anecdotes = [
-	'If it hurts, do it more often',
-	'Adding manpower to a late software project makes it later!',
-	'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-	'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-	'Premature optimization is the root of all evil.',
-	'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
 ]
 
 ReactDOM.render(
-	<App anecdotes={anecdotes} />,
-	document.getElementById('root')
+    <App anecdotes={anecdotes} />,
+    document.getElementById('root')
 )
--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -2,90 +2,90 @@ import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
 const Header = ({title}) => (
-	<h1> {title} </h1>
+    <h1> {title} </h1>
 )
 
 const Button = ({handler,text}) => (
-	<button onClick={handler}> {text} </button>
+    <button onClick={handler}> {text} </button>
 )
 
 
 const Feedback = ({strings, handlers}) => { 
 
-	return (
-		<>
-			<Header title="give feedback"/>
-			<Button handler={handlers[0]} text={strings[0]}/>
-			<Button handler={handlers[1]} text={strings[1]}/>
-			<Button handler={handlers[2]} text={strings[2]}/>
-		</>
-	)
+    return (
+        <>
+            <Header title="give feedback"/>
+            <Button handler={handlers[0]} text={strings[0]}/>
+            <Button handler={handlers[1]} text={strings[1]}/>
+            <Button handler={handlers[2]} text={strings[2]}/>
+        </>
+    )
 }
 
 
 const Statistic = ({text, num}) => (
-	<tr> 
-		<td>{text}</td> 
-		<td>{num}</td> 
-	</tr>
+    <tr> 
+        <td>{text}</td> 
+        <td>{num}</td> 
+    </tr>
 )
 
 const Statistics = ({strings,statistics}) => {
 
-	if (statistics[0] || statistics[1] || statistics[2])
-		return (
+    if (statistics[0] || statistics[1] || statistics[2])
+        return (
 
-			<>
-				<Header title="statistics"/>
-				<table>
-					<tbody>
-					<Statistic text={strings[0]} num={statistics[0]}/>
-					<Statistic text={strings[1]} num={statistics[1]}/>
-					<Statistic text={strings[2]} num={statistics[2]}/>
-					<Statistic text={strings[3]} num={statistics[3]}/>
-					<Statistic text={strings[4]} num={statistics[4]}/>
-					<Statistic text={strings[5]} num={statistics[5] + " %"}/>
-					</tbody>
-				</table>
-			</>
-		)
-	else
-		return (
+            <>
+                <Header title="statistics"/>
+                <table>
+                    <tbody>
+                        <Statistic text={strings[0]} num={statistics[0]}/>
+                        <Statistic text={strings[1]} num={statistics[1]}/>
+                        <Statistic text={strings[2]} num={statistics[2]}/>
+                        <Statistic text={strings[3]} num={statistics[3]}/>
+                        <Statistic text={strings[4]} num={statistics[4]}/>
+                        <Statistic text={strings[5]} num={statistics[5] + " %"}/>
+                    </tbody>
+                </table>
+            </>
+        )
+    else
+        return (
 
-			<>
-				<Header title="statistics"/>
-				<p> No feedback given </p>
-			</>
-		)
+            <>
+                <Header title="statistics"/>
+                <p> No feedback given </p>
+            </>
+        )
 }
 
 
 const App = () => {
-	// save clicks of each button to own state
-	const [good, setGood] = useState(0)
-	const [neutral, setNeutral] = useState(0)
-	const [bad, setBad] = useState(0)
+    // save clicks of each button to own state
+    const [good, setGood] = useState(0)
+    const [neutral, setNeutral] = useState(0)
+    const [bad, setBad] = useState(0)
 
 
-	const incGood = () => setGood(good+1)
-	const incNeutral = () => setNeutral(neutral+1)
-	const incBad = ()=> setBad(bad+1)
+    const incGood = () => setGood(good+1)
+    const incNeutral = () => setNeutral(neutral+1)
+    const incBad = ()=> setBad(bad+1)
 
-	const all = good + bad + neutral
-	const average = (good - bad)/all
-	const positive = good/all*100
-	const strings = ["good", "neutral", "bad", "all", "average", "positive"]
-	const statistics = [ good, neutral, bad, all, average, positive]
-	const handlers = [incGood, incNeutral, incBad]
+    const all = good + bad + neutral
+    const average = (good - bad)/all
+    const positive = good/all*100
+    const strings = ["good", "neutral", "bad", "all", "average", "positive"]
+    const statistics = [ good, neutral, bad, all, average, positive]
+    const handlers = [incGood, incNeutral, incBad]
 
-	return (
-		<div>
-			<Feedback strings={strings} handlers={handlers}/>
-			<Statistics strings={strings} statistics = {statistics} />
-		</div>
-	)
+    return (
+        <div>
+            <Feedback strings={strings} handlers={handlers}/>
+            <Statistics strings={strings} statistics = {statistics} />
+        </div>
+    )
 }
 
 ReactDOM.render(<App />, 
-	document.getElementById('root')
+    document.getElementById('root')
 )
