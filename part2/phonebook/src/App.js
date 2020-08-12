e34f519239bd968071a24866329f620274716d49
arskdj <68353557+arskdj@users.noreply.github.com>
2020-08-12 19:31:46 +0300

    delete confirmation

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -76,9 +76,12 @@ const Numbers = ({filteredPersons, deletePerson}) => {
 
 
 const NumberRow = ({person, deletePerson}) => {
+    const deleteHandler = () => {
+        window.confirm(`delete ${person.name} ?`) && deletePerson(person.id)
+    }
     return (
         <div>
-            <li> {person.name} {person.number} <button onClick={() => deletePerson(person.id)} > delete </button></li>
+            <li> {person.name} {person.number} <button onClick={deleteHandler} > delete </button></li>
         </div>
     )
 }
